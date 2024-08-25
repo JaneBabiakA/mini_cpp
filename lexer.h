f9c4ec3687c60382d9e96f3e9976070edcc496fe
JaneBabiakA <janefrances369@gmail.com>
2024-08-25 11:31:48 -0400

    Fleshed out lexer, added all components of a main() function.

--- lexer.h
+++ lexer.h
@@ -8,7 +8,16 @@ enum class TokenTypes {
     Token_EOF,
     Token_Ident,
     Token_Def,
-    Token_Int_Dec
+    Token_Int_Dec,
+    Token_Plus,
+    Token_Multiply,
+    Token_OpenR, //Round bracket
+    Token_CloseR, //Round bracket,
+    Token_OpenS, //Squiggle bracket
+    Token_CloseS, //Squiggle bracket,
+    Token_Semi,
+    Token_Equal,
+
 };
 
 struct Token{
@@ -16,31 +25,23 @@ struct Token{
     std::optional<std::string> value{};
 };
 
-
 class Lexer{
 
 public:
-    Lexer(std::string input){
+    explicit Lexer(std::string input){
         m_input = std::move(input);
-    }
-
-    char getNext(){
-        if(m_input[m_index] == '/' && m_input[m_index + 1] == '/'){ //TODO: check if we're exceeding the file length
-            while(m_index < m_input.length() && m_input[m_index] != '\n'){
-                m_index++;
-            }
-        }
-        else{
-            return m_input[m_index++];
-        }
     }
     std::vector<Token> lex(){
         std::vector<Token> tokens;
-        while(m_index < m_input.length()){
-            if(isalpha(m_input[m_index])){
-                std::string current = {getNext()};
-                while(m_index < m_input.length() && isalnum(m_input[m_index])){
-                    current.push_back(getNext());
+        while(fetchNext().has_value()){
+
+            //Keywords and variable names
+            if(isalpha(fetchNext().value())){
+                std::string current = {fetchNext().value()};
+                m_index++;
+                while(fetchNext().has_value() && isalnum(fetchNext().value())){
+                    current.push_back(fetchNext().value());
+                    m_index++;
                 }
                 if(current == "int"){
                     tokens.push_back({TokenTypes::Token_Int_Dec});
@@ -52,15 +53,46 @@ public:
                     tokens.push_back({TokenTypes::Token_Ident, current});
                 }
             }
-            //TODO: get negative signs/numbers and decimals working
-            if(isdigit(m_input[m_index])){
-                std::string current = {getNext()};
-                while(m_index < m_input.length() && isdigit(m_input[m_index])){
-                    current.push_back(getNext());
+
+            //Integers
+            else if(isdigit(fetchNext().value())){ //TODO: get negative signs/numbers and decimals working
+                std::string current = {fetchNext().value()};
+                m_index++;
+                while(fetchNext().has_value() && isdigit(fetchNext().value())){
+                    current.push_back(fetchNext().value());
+                    m_index++;
                 }
                 tokens.push_back({TokenTypes::Token_Int, current});
             }
-            getNext();
+
+            //Single character tokens
+            else if(fetchNext().value() == '+'){
+                tokens.push_back({TokenTypes::Token_Plus});
+                std::cout << "+";
+            }
+            else if(fetchNext().value() == '*'){
+                tokens.push_back({TokenTypes::Token_Multiply});
+                std::cout << "*";
+            }
+            else if(fetchNext().value() == '{'){
+                tokens.push_back({TokenTypes::Token_OpenS});
+            }
+            else if(fetchNext().value() == '}'){
+                tokens.push_back({TokenTypes::Token_CloseS});
+            }
+            else if(fetchNext().value() == ';'){
+                tokens.push_back({TokenTypes::Token_Semi});
+            }
+            else if(fetchNext().value() == '='){
+                tokens.push_back({TokenTypes::Token_Equal});
+            }
+            else if(fetchNext().value() == '('){
+                tokens.push_back({TokenTypes::Token_OpenR});
+            }
+            else if(fetchNext().value() == ')'){
+                tokens.push_back({TokenTypes::Token_CloseR});
+            }
+            m_index++;
         }
         return tokens;
     }
@@ -68,4 +100,17 @@ public:
 private:
     std::string m_input;
     int m_index = 0;
+    std::optional<char> fetchNext(){
+        if(m_index < m_input.length()){
+            if(m_input[m_index] == '/' && m_input[m_index + 1] == '/'){
+                while(m_index < m_input.length() && m_input[m_index] != '\n'){ // TODO: i might not need the first case?
+                    m_index++; //Skip comments
+                }
+            }
+            else{
+                return m_input[m_index];
+            }
+        }
+        return {};
+    }
 };
--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
+#include<iostream>
+#include<fstream>
+#include<sstream>
 #include"lexer.h"
 
-int main(){
-    Lexer myLexer = Lexer("hello world number 923");
+
+int main(int argc, char* argv[]){
+    if(argc != 2){
+        std::cerr << "Error" << std::endl;
+        return 1;
+    }
+    std::fstream file(argv[1]);
+    std::stringstream content;
+    content << file.rdbuf();
+    Lexer myLexer = Lexer(content.str());
     myLexer.lex();
     return 0;
 }
